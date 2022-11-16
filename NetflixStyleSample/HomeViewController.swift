2d1cc8a575594270eb0e00703ab5561ba95d55ff
최지연 <choi02647@naver.com>
2022-11-16 18:12:01 +0900

    add snapkit

---
+++ NetflixStyleSample/HomeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  HomeViewController.swift
+//  NetflixStyleSample
+//
+//  Created by Jiyeon Choi on 2022. 11. 16..
+//
+
+import UIKit
+
+class HomeViewController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //네비게이션 설정
+        navigationController?.navigationBar.backgroundColor = .clear
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.hidesBarsOnSwipe = true
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "netflix_icon"), style: .plain, target: nil, action: nil)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "person.crop.circle"), style: .plain, target: nil, action: nil)
+    }
+}
